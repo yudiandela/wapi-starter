5310604dffaea09e15562f81923b7eee121398ec
Yudi Andela <yudhi.andhela@gmail.com>
2025-06-30 08:47:49 +0700

    feat: check server route

--- src/controller/main.controller.ts
+++ src/controller/main.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/swagger';
 import { MainGuard } from './main.guard';
 import { MainService } from 'src/service/main/main.service';
+import { RedisService } from 'src/service/redis/redis.service';
 import {
   ResponseConnectSuccess,
   ResponseContactSuccess,
@@ -26,11 +27,26 @@ import { BodySendMessage, BodySendStatus, BodyConnect } from './main.dto';
 @ApiTags('Main Controller')
 @Controller()
 export class MainController {
-  constructor(private mainService: MainService) {}
+  constructor(
+    private mainService: MainService,
+    private readonly redisService: RedisService
+  ) {}
 
-  @Get('/')
-  homePage() {
-    return 'Hello World!';
+  @Get('/check')
+  async check() {
+    const start = process.hrtime();
+
+    const redisStatus = this.redisService.isConnected();
+    const diff = process.hrtime(start);
+    const responseTimeMs = (diff[0] * 1000 + diff[1] / 1e6).toFixed(2);
+
+    const message = redisStatus ? 'Healthy!' : 'Degraded: Redis down';
+
+    return {
+      message: message,
+      response_time: `${responseTimeMs}ms`,
+      redis: redisStatus ? 'connected' : 'disconnected',
+    };
   }
 
   @Get('qr')
